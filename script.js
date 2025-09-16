8c3c714ee5a58dd448545e9fe6656e748ee55477
carltonjohnson1 <carltonj1@hotmail.com>
2025-09-16 18:57:01 +0100

    initial commit to get structure done

---
+++ script.js
@@ -0,0 +1,4 @@
+const passwordField = document.getElementById("password");
+const generateBtn = document.getElementById("generateBtn");
+const copyBtn = document.getElementById("copyBtn");
+const toast = document.getElementById("toast");
